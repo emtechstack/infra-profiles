1bf004fda1449a1cd6d297d6aaa9382b565dff5e
erfanvaredi <erfanvaredi@outlook.com>
2024-07-01 16:54:53 +0400

    refactor: fix typo in DATABASE_URL variable assignment

--- profiles/postgresql-qdrant-redis/utils/config.py
+++ profiles/postgresql-qdrant-redis/utils/config.py
@@ -3,4 +3,4 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = f"postgresql+asyncpg://{os.getenv('POSTGRE__DATABASE_USERNAME')}:{os.getenv('POSTGRE__DATABASE_PASSWORD')}@{os.getenv('POSTGRE__DATABASE_HOST')}/{os.GETENV('POSTGRE__DATABASE_NAME')}"
+DATABASE_URL = f"postgresql+asyncpg://{os.getenv('POSTGRE__DATABASE_USERNAME')}:{os.getenv('POSTGRE__DATABASE_PASSWORD')}@{os.getenv('POSTGRE__DATABASE_HOST')}/{os.getenv('POSTGRE__DATABASE_NAME')}"
